2d9dd53b14836ad43ca9c50834d59267cfacb3b7
DennsHartstock <dennis.hartstock@web.de>
2022-11-23 07:13:42 +0100

    create note list

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.todolist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout llNotes;
+    private FloatingActionButton btAddNote;
+
+    private ArrayList<Note> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            Note note = new Note(i, "Note " + i, random.nextInt(3));
+            notes.add(note);
+        }
+        showNotes();
+    }
+
+    private void initViews() {
+        llNotes = findViewById(R.id.llNotes);
+        btAddNote = findViewById(R.id.btAddNote);
+    }
+
+    private void showNotes() {
+
+        for (Note note : notes) {
+            View view = getLayoutInflater().inflate(R.layout.note_item, llNotes, false);
+            TextView tvNote = view.findViewById(R.id.tvNote);
+            tvNote.setText(note.getText());
+
+            int colorResId;
+            switch (note.getPriority()) {
+                case 0:
+                    colorResId = android.R.color.holo_green_light;
+                    break;
+                case 1:
+                    colorResId = android.R.color.holo_orange_light;
+                    break;
+                case 2:
+                    colorResId = android.R.color.holo_red_light;
+                    break;
+                default:
+                    colorResId = R.color.teal_200;
+            }
+
+            int color = ContextCompat.getColor(this, colorResId);
+            tvNote.setBackgroundColor(color);
+
+            llNotes.addView(view);
+        }
     }
 }
